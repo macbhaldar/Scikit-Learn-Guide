02499f666bab8337d14e847354bd8f1141d0d118
Maksud Bhaldar <macbhaldar@gmail.com>
2022-06-19 21:35:32 +0530

    Update cross-validation_test-train.py

--- cross-validation_test-train.py
+++ cross-validation_test-train.py
@@ -85,3 +85,70 @@ from sklearn.model_selection import cross_val_score
 
 scores = cross_val_score(regressor, data, target)
 scores
+
+# compute the error on the training set
+import pandas as pd
+from sklearn.model_selection import cross_validate, ShuffleSplit
+
+cv = ShuffleSplit(n_splits=30, test_size=0.2)
+cv_results = cross_validate(regressor, data, target,
+                            cv=cv, scoring="neg_mean_absolute_error",
+                            return_train_score=True, n_jobs=2)
+cv_results = pd.DataFrame(cv_results)
+
+scores = pd.DataFrame()
+scores[["train error", "test error"]] = -cv_results[
+    ["train_score", "test_score"]]
+import matplotlib.pyplot as plt
+
+scores.plot.hist(bins=50, edgecolor="black")
+plt.xlabel("Mean absolute error (k$)")
+_ = plt.title("Train and test errors distribution via cross-validation")
+
+# Validation curve
+from sklearn.model_selection import validation_curve
+
+max_depth = [1, 5, 10, 15, 20, 25]
+train_scores, test_scores = validation_curve(
+    regressor, data, target, param_name="max_depth", param_range=max_depth,
+    cv=cv, scoring="neg_mean_absolute_error", n_jobs=2)
+train_errors, test_errors = -train_scores, -test_scores
+
+plt.plot(max_depth, train_errors.mean(axis=1), label="Training error")
+plt.plot(max_depth, test_errors.mean(axis=1), label="Testing error")
+plt.legend()
+
+plt.xlabel("Maximum depth of decision tree")
+plt.ylabel("Mean absolute error (k$)")
+_ = plt.title("Validation curve for decision tree")
+
+# learning curve
+import numpy as np
+train_sizes = np.linspace(0.1, 1.0, num=5, endpoint=True)
+train_sizes
+
+from sklearn.model_selection import ShuffleSplit
+
+cv = ShuffleSplit(n_splits=30, test_size=0.2)
+
+from sklearn.model_selection import learning_curve
+
+results = learning_curve(
+    regressor, data, target, train_sizes=train_sizes, cv=cv,
+    scoring="neg_mean_absolute_error", n_jobs=2)
+train_size, train_scores, test_scores = results[:3]
+# Convert the scores into errors
+train_errors, test_errors = -train_scores, -test_scores
+
+import matplotlib.pyplot as plt
+
+plt.errorbar(train_size, train_errors.mean(axis=1),
+             yerr=train_errors.std(axis=1), label="Training error")
+plt.errorbar(train_size, test_errors.mean(axis=1),
+             yerr=test_errors.std(axis=1), label="Testing error")
+plt.legend()
+
+plt.xscale("log")
+plt.xlabel("Number of samples in the training set")
+plt.ylabel("Mean absolute error (k$)")
+_ = plt.title("Learning curve for decision tree")
